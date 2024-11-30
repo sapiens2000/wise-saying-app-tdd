5ab12ae3450da798ee59a92364d4df4b996b295e
ByeongGyu Jeon <cube9928@naver.com>
2024-11-30 14:58:18 +0900

    1단계 테스트 성공 & 테TestUtil 추가

--- src/main/java/com/ex/App.java
+++ src/main/java/com/ex/App.java
@@ -1,5 +1,7 @@
 package com.ex;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         App.run();
@@ -7,7 +9,6 @@ public class App {
 
     public static void run(){
         System.out.println("== 명언 앱 ==");
-
         System.out.println("명령) ");
     }
 }
--- src/test/java/com/ex/AppTest.java
+++ src/test/java/com/ex/AppTest.java
@@ -1,18 +1,24 @@
 package com.ex;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import org.junit.jupiter.api.*;
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
 
+
     @DisplayName("1단계 테스트")
     @Test
     void step1(){
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
         App.run();
+        assertThat(output.toString())
+                .contains("== 명언 앱 ==")
+                .contains("명령) ");
+
+        TestUtil.clearSetOutToByteArray(output);
     }
 }
