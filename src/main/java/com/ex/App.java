4916dcf0ac6699b7c6d5145f309dd8c1d076a2a1
ByeongGyu Jeon <cube9928@naver.com>
2024-12-01 21:45:37 +0900

    5단계 테스트 green blue

--- src/main/java/com/ex/App.java
+++ src/main/java/com/ex/App.java
@@ -1,5 +1,7 @@
 package com.ex;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,24 +12,38 @@ public class App {
 
     public void run(Scanner sc){
         System.out.println("== 명언 앱 ==");
+
         Long id = 1L;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while(true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("등록")) {
+            if(cmd.equals("종료")){
+                break;
+            }
+            else if(cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(WiseSaying cur : wiseSayings.reversed()){
+                    System.out.println(cur);
+                }
+            }
+
+            else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String wiseSaying = sc.nextLine();
 
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
 
+                // 유효성 검증하기
+                
+                wiseSayings.add(new WiseSaying(id, wiseSaying, author));
                 System.out.println(id++ + "번 명언이 등록되었습니다.");
             }
-            else if(cmd.equals("종료")){
-                break;
-            }
+
         }
 
     }
---
+++ src/main/java/com/ex/WiseSaying.java
@@ -0,0 +1,57 @@
+package com.ex;
+
+import java.util.Objects;
+
+public class WiseSaying {
+    Long id;
+    String content;
+    String author;
+
+    public WiseSaying(Long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d / %s / %s\n", id, author, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WiseSaying that = (WiseSaying) o;
+        return Objects.equals(id, that.id) && Objects.equals(content, that.content) && Objects.equals(author, that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, author);
+    }
+}
