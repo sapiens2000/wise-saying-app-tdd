8d4ad214abecc1f6335bf994098770b1588abdca
ByeongGyu Jeon <cube9928@naver.com>
2024-12-01 21:48:52 +0900

    6단계 테스트 red

--- src/test/java/com/ex/AppTest.java
+++ src/test/java/com/ex/AppTest.java
@@ -9,6 +9,14 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
+    private static final ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+
+    @AfterEach()
+    void clear(){
+        TestUtil.clearSetOutToByteArray(output);
+    }
+
     @DisplayName("1단계 테스트")
     @Test
     void step1() {
@@ -49,7 +57,7 @@ class AppTest {
         TestUtil.clearSetOutToByteArray(output);
     }
 
-    @DisplayName("3단계 테스트")
+    @DisplayName("3단계 등록 시 명언번호 노출")
     @Test
     void step3(){
         String cmd = """
@@ -73,7 +81,7 @@ class AppTest {
         TestUtil.clearSetOutToByteArray(output);
     }
 
-    @DisplayName("4단계 테스트")
+    @DisplayName("4단계 동록 시 마다 명언 번호 증가")
     @Test
     void step4(){
         String cmd = """
@@ -101,7 +109,7 @@ class AppTest {
         TestUtil.clearSetOutToByteArray(output);
     }
 
-    @DisplayName("5단계 테스트")
+    @DisplayName("5단계 목록 테스트")
     @Test
     void step5(){
         String cmd = """
@@ -129,4 +137,31 @@ class AppTest {
 
         TestUtil.clearSetOutToByteArray(output);
     }
+
+    @DisplayName("6단계 명언 삭제")
+    @Test
+    void step6(){
+        String cmd = """
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                삭제?id=1
+                종료
+                """;
+
+        Scanner sc = TestUtil.genScanner(cmd);
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        App app = new App();
+        app.run(sc);
+
+        assertThat(output.toString())
+                .contains("1번 명언이 삭제되었습니다.");
+        TestUtil.clearSetOutToByteArray(output);
+    }
+
 }
