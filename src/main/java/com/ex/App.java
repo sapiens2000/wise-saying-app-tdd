8d9c6ba647cd72901bb6b6809c1fcf8086a16a88
ByeongGyu Jeon <cube9928@naver.com>
2024-12-01 20:34:17 +0900

    2단계 테스트 성공

--- src/main/java/com/ex/App.java
+++ src/main/java/com/ex/App.java
@@ -4,11 +4,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        App.run();
+        App app = new App();
+        app.run(new Scanner(System.in));
     }
 
-    public static void run(){
+    public void run(Scanner sc){
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
+
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = sc.nextLine();
+
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
+            else if(cmd.equals("종료")){
+                break;
+            }
+        }
+
     }
 }
--- src/test/java/com/ex/AppTest.java
+++ src/test/java/com/ex/AppTest.java
@@ -1,23 +1,50 @@
 package com.ex;
 
 import org.junit.jupiter.api.*;
+
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
-
-
     @DisplayName("1단계 테스트")
     @Test
-    void step1(){
+    void step1() {
+        Scanner sc = TestUtil.genScanner("종료");
         ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
-        App.run();
+        App app = new App();
+        app.run(sc);
+
         assertThat(output.toString())
                 .contains("== 명언 앱 ==")
                 .contains("명령) ");
+
+        TestUtil.clearSetOutToByteArray(output);
+    }
+
+    @DisplayName("2단계 테스트")
+    @Test
+    void step2() {
+        String cmd = """
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """;
+
+        Scanner sc = TestUtil.genScanner(cmd);
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+
+        App app = new App();
+        app.run(sc);
+
+        assertThat(output.toString())
+                .contains("명언 : ")
+                .contains("작가 : ");
 
         TestUtil.clearSetOutToByteArray(output);
     }
